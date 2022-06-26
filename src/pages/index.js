49e1b96cc5d9165e08c4133ef397d0e4e3391dae
Alexander <alexander.nerz@web.de>
2022-06-26 21:23:11 +0200

    rename Folder

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,7 @@ import Contact from '../components/Contact';
 import Footer from '../components/Footer';
 import { homeObjOne} from '../components/About/Data';
 import Navbar from '../components/Navbar';
-import Sidebar from '../components/Sidebar';
+import Sidebar from '../components/Sidebar'
 import Intro from '../components/Intro';
 import Projects from '../components/Projects';
 import About from "../components/About";
@@ -30,4 +30,4 @@ const Home = () => {
   )
 }
 
-export default Home
+export default Home;
