eaf6495b2777257df8da36207dff39b8cdfd2600
Alexander <alexander.nerz@web.de>
2022-06-27 17:24:24 +0200

    add index to import Navbar

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,13 +2,11 @@ import React, {useState} from 'react';
 import Contact from '../components/Contact';
 import Footer from '../components/Footer';
 import { homeObjOne} from '../components/About/Data';
-
 import Intro from '../components/Intro';
 import Projects from '../components/Projects';
 import About from "../components/About";
-import Navbar from '../components/Navbar';
-
-import Sidebar from '../components/Sidebar' ;
+import Navbar from '../components/Navbar/index';
+import Sidebar from '../components/Sidebar/index';
 
 const Home = () => {
   const [isOpen, setIsOpen] = useState(false)
