9f88c5f5b68410a5ef8c50a51c4d4a040ea792a9
Alexander <alexander.nerz@web.de>
2022-07-23 16:54:21 +0200

    update Style contact me

--- src/components/Contact/ContactElements.js
+++ src/components/Contact/ContactElements.js
@@ -13,17 +13,34 @@ export const ContactContainer = styled.div`
 `;
 export const ContactContent = styled.div`
   z-index: 3;
-  max-width: 1200px;
   position: absolute;
   padding: 8px 24px;
+  height: 400px;
+  width: 600px;
   display: flex;
   flex-direction: column;
   align-items: center;
+  background: #fff;
+  box-shadow:  0 70px 40px -20px rgba(0, 0, 0, 0.2);
+  border-radius: 10px;
+
+  @media screen and (max-width: 768px) {
+    height: 350px;
+    width: 520px;
+  }
+  @media screen and (max-width: 580px) {
+    height: 380px;
+    width: 460px;
+  }
+  @media screen and (max-width: 480px) {
+    height: 380px;
+    width: 300px;
+  }
 `;
 export const ContactH1 = styled.h1`
   font-size: 2.5rem;
   color: #000;
-  margin-bottom: 100px;
+  margin-bottom: 150px;
   font-weight: bold;
 `;
 export const ContactP = styled.p`
